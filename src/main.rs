96389549bd1ba4fff4188a4e6c33d99599627ff3
NullUsxr <nullusxr@tuta.io>
2022-07-22 16:09:45 -0600

    Temp files until bootloader finished

---
+++ src/main.rs
@@ -0,0 +1,3 @@
+// NOTE: Pass off unique auth key to bootloader and perform handshake to verify booting to legit kernel
+//  This means we will have to implement further protections in boot.asm before publishing
+// Currently cannot work on kernel until bootloader is finished
